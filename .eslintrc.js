b0328b883337ac519607e00a742aeb48c78d6fd4
Paweł Żurawka <pzurawka@gmail.com>
2022-06-30 14:28:01 +0200

    Add eslint rules to config file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,32 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
+    'no-alert': 'off', // wyłącza ostrzeżenia o alertach i promptach
+    'linebreak-style': ['error', 'windows'],
+    'no-console': 'off', // ostrzeżenia o użyciu console.log
+    'max-len': 'off', // wyłącza ostrzeżenie o maksymalnej długości linii (100znaków)
+    'arrow-parens': [
+      // brak nawiasów w przypadku jednego parametru funkcji
+      2,
+      'as-needed',
+    ],
+    'func-names': [
+      // wyłącza ostrzeżenie o anonimowych funkcjach
+      'error',
+      'never',
+    ],
+    'no-restricted-syntax': [
+      'error',
+      // "FunctionExpression",
+      // "WithStatement",
+      "BinaryExpression[operator='in']",
+    ],
   },
 };
