18b1154ea06ea23adf23260b76b30fe1a2a4e6fd
Paweł Żurawka <pzurawka@gmail.com>
2022-07-04 18:54:34 +0200

    Add types, model and schema.

---
+++ models/post.model.ts
@@ -0,0 +1,10 @@
+import { PostInterface } from '../types';
+import { Schema, model } from 'mongoose';
+
+const postSchema = new Schema<PostInterface>({
+  title: { type: 'String', required: true },
+  author: { type: 'String', required: true },
+  content: { type: 'String', required: true },
+});
+
+export const Post = model<PostInterface>('Post', postSchema);
---
+++ types/post/post.ts
@@ -0,0 +1,5 @@
+export interface PostInterface {
+  title: string;
+  author: string;
+  content: string;
+}
