894c00b4a0f92e844b3b5300b34c500f96eaeebe
Paweł Żurawka <pzurawka@gmail.com>
2022-07-12 22:04:15 +0200

    Add uuid.

--- models/post.model.ts
+++ models/post.model.ts
@@ -1,8 +1,10 @@
 import { PostInterface } from '../types';
 import { Schema, model } from 'mongoose';
+import { v4 as uuid } from 'uuid';
 
 const postSchema = new Schema<PostInterface>(
   {
+    id: { type: String, required: true, unique: true, default: uuid },
     title: { type: String, required: true, unique: true, minlength: 3, maxlength: 100 },
     content: { type: String, required: true, minlength: 2, maxlength: 2000 },
     photo: { type: String, default: '' },
--- types/post/post.ts
+++ types/post/post.ts
@@ -1,6 +1,7 @@
 import { Document, Types } from 'mongoose';
 
 export interface PostInterface extends Document {
+  id: string;
   title: string;
   content: string;
   photo?: string;
