cf61b1d3cb17e26aac070a00a70af18803680dab
Paweł Żurawka <pzurawka@gmail.com>
2022-07-12 22:48:30 +0200

    Add uuid to categories schema.

--- models/category.model.ts
+++ models/category.model.ts
@@ -1,8 +1,10 @@
 import { CategoryInterface } from '../types';
 import { Schema, model } from 'mongoose';
+import { v4 as uuid } from 'uuid';
 
 const categorySchema = new Schema<CategoryInterface>(
   {
+    id: { type: String, required: true, unique: true, default: uuid },
     name: { type: String, required: true, minlength: 3, maxlength: 50 },
   },
   { timestamps: true }
