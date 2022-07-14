925e26e42b0f8661b6f342cd31f136fd92b52507
Paweł Żurawka <pzurawka@gmail.com>
2022-07-14 09:17:50 +0200

    Change post schema.

--- models/post.model.ts
+++ models/post.model.ts
@@ -5,10 +5,10 @@ import { v4 as uuid } from 'uuid';
 const postSchema = new Schema<PostInterface>(
   {
     id: { type: String, required: true, unique: true, default: uuid },
-    title: { type: String, required: true, unique: true, minlength: 3, maxlength: 100 },
-    content: { type: String, required: true, minlength: 2, maxlength: 2000 },
+    title: { type: String, required: true, minlength: 3, maxlength: 100 },
+    content: { type: String, required: true, minlength: 3, maxlength: 2000 },
     photo: { type: String, default: '' },
-    username: { type: String, required: true, minlength: 3, maxlength: 50 },
+    username: { type: String, required: true, minlength: 2, maxlength: 50 },
     categories: [String],
   },
   { timestamps: true }
